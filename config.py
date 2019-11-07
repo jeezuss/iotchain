3c97415ff96490add5b4a5d15f8c2d553a47ba73
jeezuss <42808839+jeezuss@users.noreply.github.com>
2019-11-07 17:36:28 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+CHAINDATA_DIR = 'chaindata/'
+NUM_ZEROS = 3
+
+PEERS = [
+    'http://localhost:5000/'
+    ]
+
+BLOCK_VAR_CONVERSIONS = {'index': int, 'nonce': int, 'hash': str, 'prev_hash': str, 'timestamp': int}
+
+STANDARD_ROUNDS = 10
+
